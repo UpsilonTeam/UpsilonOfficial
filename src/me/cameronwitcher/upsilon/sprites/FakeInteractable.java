9443d2ec6d4085f4447f3ea63f05f98bfa8ddb0b
Cameron Witcher <CameronWitcher2017@gmail.com>
2016-01-27 12:23:58 -0600

    ADD: Added fake interactables

---
+++ src/me/cameronwitcher/upsilon/sprites/FakeInteractable.java
@@ -0,0 +1,34 @@
+package me.cameronwitcher.upsilon.sprites;
+
+import me.cameronwitcher.upsilon.spriteutils.Interactable;
+import me.cameronwitcher.upsilon.spriteutils.SpriteType;
+import me.cameronwitcher.upsilon.spriteutils.State;
+import res.Texture;
+
+public class FakeInteractable extends Interactable {
+
+	
+	private SpriteType type;
+	private State state;
+	
+	public FakeInteractable(int x, int y, SpriteType type, State state) {
+		super(x, y);
+		this.type = type;
+		this.state = state;
+		init();
+	}
+	
+	public void init(){
+		switch(type){
+		case WALL:
+			loadImage(Texture.loadTexture("wall/wall_" + state.toString().toLowerCase() + ".png"));
+			getImageDimensions();
+		default:
+			break;
+		}
+	}
+	public SpriteType getType(){
+		return type;
+	}
+
+}
